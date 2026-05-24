652c6883f77867f53e3de4842a8dac63d96d983e
Prashant Kumar <prashant2009kr@gmail.com>
2026-05-24 23:27:57 +0530

    fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Cormorant_Garamond, DM_Sans } from "next/font/google";
-import { ThemeProvider } from "next-themes";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Toaster } from "@/components/ui/sonner";
@@ -38,12 +37,10 @@ export default function RootLayout({
       className={`${cormorant.variable} ${dmSans.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col" suppressHydrationWarning>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
           <Navbar />
           <main className="flex-1">{children}</main>
           <Footer />
           <Toaster position="bottom-right" richColors />
-        </ThemeProvider>
       </body>
     </html>
   );
